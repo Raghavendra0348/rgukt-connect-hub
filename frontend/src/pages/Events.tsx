adb73efb163b63c0d3048a541b1f3304cd2943db
A Raghavendra <arellaraghavendra@gmail.com>
2025-12-21 12:12:47 +0530

    updated the code

---
+++ frontend/src/pages/Events.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Events() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Events</h1>
+      <p>Upcoming events and activities will be listed here soon.</p>
+    </div>
+  );
+}
